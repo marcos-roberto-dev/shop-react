ca9c486de59f2389bf026d987d409aeedd961120
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-07-21 18:03:47 -0300

    :tada: start project shop-react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GlobalStyles } from "@/styles/global";
+import type { AppProps } from "next/app";
+
+GlobalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
